b8bbac0d6712a671bc8144cd9b5676fcb97b73a8
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-06-23 23:40:35 +0900

    useClick

--- useInput/index.js
+++ useInput/index.js
@@ -6,3 +6,38 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+//useClick React 16.8v이상버전
+import { useEffect, useState, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useClick = (onClick) => {
+  const element = useRef();
+  const useClick = (onClick) => {
+    const element = useRef();
+    useEffect(() => {
+      if (typeof onClick !== "function") {
+        return;
+      }
+      if (element.current) {
+        element.current.addEventListener("click", onClick);
+      }
+      return () => {
+        if (element.current) {
+          element.current.removeEventListener("click", onClick);
+        }
+      };
+    }, []);
+    return typeof onClick !== "function" ? element : undefined;
+  };
+};
+
+export default function App() {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+}
