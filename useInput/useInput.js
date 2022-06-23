c025057995ec0284036226867825881ad1373b45
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-06-23 21:30:07 +0900

    useTitle

--- useInput/useInput.js
+++ useInput/useInput.js
@@ -94,3 +94,27 @@ export default function App() {
   );
 }
 
+
+//useTitle
+import { useEffect, useState } from "react";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+export default function App() {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return (
+    <div className="App">
+      <div>Hi</div>
+    </div>
+  );
+}
+
