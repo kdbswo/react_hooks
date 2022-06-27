5aeb6be154d455c750cecb704781d84f41d4e62a
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-06-28 02:41:15 +0900

    useAxios

---
+++ useAxios/App.js
@@ -0,0 +1,13 @@
+export default function App() {
+  const { loading, error, data, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
+  });
+
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+}
